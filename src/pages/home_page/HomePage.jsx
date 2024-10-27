7320d942add8ee722a90c0049945fd6aaf6a2d76
jhojangit <jhojanblackout@gmail.com>
2024-10-27 08:18:35 -0500

    Fixed folders name lowercase

--- src/pages/home_page/HomePage.jsx
+++ src/pages/home_page/HomePage.jsx
@@ -4,8 +4,8 @@ import FormCreateAdvisor from '../../components/formCreateAdvisor/FormCreateAdvi
 import FormCreateFaculty from '../../components/formCreateFaculty/FormCreateFaculty'
 import FormCreateProgram from '../../components/formCreateProgram/FormCreateProgram'
 import FormCreateCourse from '../../components/formCreateCourse/FormCreateCourse'
-import FormCreatePhasesPage from '../FormCreatePhases_page/FormCreatePhases_page'
-import CourseDetails from '../../components/CourseDetails/CourseDetails'
+import FormCreatePhasesPage from '../formCreatePhases_page/FormCreatePhases_page'
+import CourseDetails from '../../components/courseDetails/CourseDetails'
 
 
 const HomePage = () => {
